ac04caf8dac193cba146bcc5899565613742348d
DeepaPrasanna <anneraj73@gmail.com>
2024-05-18 21:26:12 +0530

    test

--- src/app/actions/auth.tsx
+++ src/app/actions/auth.tsx
@@ -5,10 +5,22 @@ import { createClient } from "../utils/supabase/server";
 
 export async function login() {
   const supabase = createClient();
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "https://teenyurl.in/auth/callback",
+      redirectTo: `${getURL()}/auth/callback`,
     },
   });
 
